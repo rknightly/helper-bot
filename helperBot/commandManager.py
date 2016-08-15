74ceb6cd9884072dad68feaa8bc77e6eeac6c705
Ryan Knightly <reknightly@gmail.com>
2016-08-15 17:16:57 -0500

    Added more websites

--- helperBot/commandManager.py
+++ helperBot/commandManager.py
@@ -2,13 +2,41 @@ import webbrowser
 
 class CommandManager:
     def __init__(self):
-        self.command_actions = {"pandora": pandora}
+        self.command_actions = {"pandora": pandora, 
+        "gmail": gmail, "drive": drive, "chess": chess,
+        "google": google, "setup": setup}
 
     def manageCommand(self, commandName):
         self.command_actions[commandName]()
+        print("EZ mon$y! What else can I do for you?")
 
+    def list_commands(self):
+        commands = [command for command in self.command_actions.keys]
+        print(str(commands))
+
+
+def open_website(url):
+    protocol = 'http://'
+    full_url = protocol + url
+    webbrowser.open_new(full_url)
 
 def pandora():
-    url = "http://pandora.com/"
-    webbrowser.open_new(url)
-    
+    open_website("pandora.com")
+
+def gmail():
+    open_website("mail.google.com")
+
+def drive():
+    open_website("drive.google.com")
+
+def chess():
+    open_website("chess.com")
+
+def google():
+    open_website("google.com")
+
+def setup():
+    drive()
+    google()
+    pandora()
+
